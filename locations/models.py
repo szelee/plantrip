d3688d879a1e17a55f7682dc6491572b98efc636
szelee <sze.lee.lai@gmail.com>
2015-02-01 09:48:43 +0000

    initial change for location

--- locations/models.py
+++ locations/models.py
@@ -1,8 +1,27 @@
 from django.db import models
 
 # Create your models here.
-TYPE_CHOICES = [
-    ('flight', 'Flight'),
-    ('hotel', 'Hotel'),
-    ('car', 'Car')
+PREF_CHOICES = [
+    ('vintage_point', 'Vintage Point'),
+    ('popular', 'Popular'),
+    ('museum', 'Museum'),
+    ('restaurant', 'Restaurant'),
+    ('park', 'Park'),
+    ('kids', 'Kids Friendly'),
+    ('shopping', 'Shopping'),
+    ('pub', 'Local Pub'),
+    ('cafe', 'Cafe'),
+    ('group', 'Group travel'),
+    ('fast_food', 'Quick bite'),
+    ('deal', 'Special Deal'),
+    ('magic', 'Something Magic')
 ]
+
+
+class Location(models.Model):
+    id = models.AutoField(primary_key=True)
+    Name = models.CharField(max_length = 255)
+    coordinate = models.CharField(max_length = 255)
+    Address = []
+    subject = models.CharField(max_length=150)
+    message =  models.TextField()
