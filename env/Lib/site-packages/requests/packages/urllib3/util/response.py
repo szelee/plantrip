7098f956c9d8e8ccda4b6cda3ef1386c8402293b
szelee <sze.lee.lai@gmail.com>
2015-02-09 13:04:15 +0000

    environment setting

--- env/Lib/site-packages/requests/packages/urllib3/util/response.py
+++ env/Lib/site-packages/requests/packages/urllib3/util/response.py
@@ -5,18 +5,9 @@ def is_fp_closed(obj):
     :param obj:
         The file-like object to check.
     """
-
-    try:
-        # Check via the official file-like-object way.
-        return obj.closed
-    except AttributeError:
-        pass
-
-    try:
-        # Check if the object is a container for another file-like object that
-        # gets released on exhaustion (e.g. HTTPResponse).
+    if hasattr(obj, 'fp'):
+        # Object is a container for another file-like object that gets released
+        # on exhaustion (e.g. HTTPResponse)
         return obj.fp is None
-    except AttributeError:
-        pass
 
-    raise ValueError("Unable to determine whether fp is closed.")
+    return obj.closed
