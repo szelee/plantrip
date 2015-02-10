ac44a985ba76e04805026d911d75b2db84ae17f2
szelee <sze.lee.lai@gmail.com>
2015-02-10 08:02:29 +0000

    update data processing for foursquare

--- locations/__init__.py
+++ locations/__init__.py
@@ -2,6 +2,7 @@ FSQR_VENUE_URL = "https://api.foursquare.com/v2/venues/search"
 FSQR_VENUE_EXPLORE = "https://api.foursquare.com/v2/venues/search/explore"
 GPLACES_BASIC_URL = "https://maps.googleapis.com/maps/api/place/search/json"
 
+
 LONDON_GEO = '51.500152,-0.126236'
 
 FACTUAL_KEY='RdM1GXUi0Gb7qT1t5flTqMgVgWmIJDlELTFkgbiE'
--- locations/getlocations.py
+++ locations/getlocations.py
@@ -5,13 +5,13 @@ from datetime import date, datetime
 from django.http import HttpResponseRedirect
 from factual import Factual
 
-#fsqr_venue_url = "https://api.foursquare.com/v2/venues/search"
 today_date = date.today()
 
 def fsqr_api(location):
     #save the result back to json file 
     #client id and client secret are stored in socialapp
     #https://api.foursquare.com/v2/venues/search?ll=40.7,-74&client_id=CLIENT_ID&client_secret=CLIENT_SECRET&v=YYYYMMDD
+    #https://api.foursquare.com/v2/venues/explore?v=20131016&ll=51.500152%2C-0.126236&section=food&novelty=new&venuePhotos=1
 
     try:
         fsqr_app = SocialApp.objects.get(provider='foursquare')
@@ -67,27 +67,28 @@ def get_location(provider, location):
         data = gplaces_api(location)
         shops = data['results']
 
-    elif provider == 'factual':
+    if provider == 'factual':
         data = factual_api(location)
         shops = data
 
     return shops
 
 def factual_api(location):
-    factual = Factual(FACTUAL_KEY, FACTUAL_SECRET)
+    factual = Factual(FACTUAL_KEY, FACTUAL_SECRET, timeout=60.0)
     location = 'london'
     #SLL - temporary commented schema as prototype will get the information immediately from the factaul db
     #s = factual.table('places').schema()
     
-    places = factual.table('places')
-    popular = places.filters({'$and':[{'category_ids':{'$includes_any':[107,308,371]}},{'locality': location}]}).sort({'placerank':800}).data()
-    restaurant = places.filters({'$and':[{'category_ids':{'$includes_any':[312,338]}},{'locality': location}]}).sort({'placerank':1000}).data()
-    to_do = places.filters({'$and':[{'category_ids':{'$includes_any':[317,147,169]}},{'locality': location}]}).sort({'placerank':1000}).data()
+    #places = factual.table('places')
+    #popular = places.filters({'$and':[{'category_ids':{'$includes_any':[107,308,371]}},{'category_ids': {'$excludes_any':[62,177,430, 347]}},{'locality': location}]}).sort({'placerank':1000}).data()
+    #restaurant = places.filters({'$and':[{'category_ids':{'$includes_any':[312,347, 338]}},{'locality': location}]}).sort({'placerank':1000}).data()
+    #to_do = places.filters({'$and':[{'category_ids':{'$includes_any':[317,147,169]}},{'locality': location}]}).sort({'placerank':1000}).data()
 
     #f = open('locations\popular_places.json', 'r')
     #popular = json.load(f)
-    #restaurant = {}
-    #to_do = {}
+    popular = process_photos(explore_fsqr())
+    restaurant = {}
+    to_do = {}
 
     result = {}
     result['popular'] = popular
@@ -96,8 +97,38 @@ def factual_api(location):
     
     return result
 
+def download_factual_category():
+    factual = Factual(FACTUAL_KEY, FACTUAL_SECRET, timeout=60.0)
+    places_schema = factual.table('places').schema()
+    
+    filename = 'locations/factual_cat_schema.json'
+    with open(filename, 'w') as outfile:
+        json.dump(places_schema, outfile)
+        outfile.close()
 
+def explore_fsqr():
+    
+    f = open('locations/fsqure_explore_london.json', 'r')
+    places = json.load(f, 'ISO-8859-1')
+    f.close()
 
+    return places
 
+def trending_fsqr():
 
+    f = open('locations/fsqr_trending.json', 'r')
+    places = json.load(f, 'ISO-8859-1')
+    f.close()
 
+    return places
+
+def process_photos(places):
+
+    if places:
+        for place in places:
+            if place['venue']['featuredPhotos']:
+                place['venue']['photos']['photoBig'] = place['venue']['featuredPhotos']['items'][0]['prefix'] + "110x110" + place['venue']['featuredPhotos']['items'][0]['suffix']
+            elif place['venue']['photos']:
+                place['venue']['photos']['photoBig'] = place['venue']['photos']['items'][0]['prefix'] + "110x110" + place['venue']['photos']['items'][0]['suffix']
+
+    return places
